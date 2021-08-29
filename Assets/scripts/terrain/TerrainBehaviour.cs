6f7a5b939034796caef214bbc2ff1fc86ab1b756
Dylan <30143500+DylanFrancis@users.noreply.github.com>
2021-08-29 18:49:30 +0200

    init and we have a moving cude

---
+++ Assets/scripts/terrain/TerrainBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainBehaviour : MonoBehaviour
+{
+    public Camera camera;
+    public PlayerMovement playerMovement;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+
+            Debug.DrawRay(ray.origin, ray.direction, Color.blue);
+            
+            if (Physics.Raycast(ray.origin, ray.direction, out hit));
+            {
+                playerMovement.move(hit.point);
+            }
+        }        
+    }
+}
